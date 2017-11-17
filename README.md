6e7559cb8ccc7aec065a9b5e169efea1643719af
amaurel <alexandre.maurel@gmail.com>
2017-11-17 12:47:31 +0100

    packrat init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Principal Components Analysis
+
+```
+https://github.com/tercen/tsne_operator.git
+```
+
+```R
+
+packrat::off()
+unlink('packrat', recursive = TRUE)
+
+devtools::install_github("tercen/TSON", ref = "1.4-rtson", subdir="rtson", upgrade_dependencies = TRUE)
+devtools::install_github("tercen/teRcen", ref = "0.4.7", upgrade_dependencies = TRUE)
+ 
+remove.packages("tercen", lib = "./packrat/lib/x86_64-pc-linux-gnu/3.3.2")
+remove.packages("rtson", lib = "./packrat/lib/x86_64-pc-linux-gnu/3.3.2")
+  
+packrat::init(options = list(
+  use.cache = TRUE
+  ))
+```
+
+```R
+
+packrat::status()
+packrat::snapshot()
+
+packrat::off()
+
+packrat::bundle(include.src=FALSE, overwrite = TRUE, include.bundles=FALSE)
+
+```
+
---
+++ main.R
@@ -0,0 +1,35 @@
+library(tercen)
+library(dplyr)
+library(reshape2)
+library(Rtsne)
+
+# Sets seed for reproducibility
+set.seed(42)
+
+(ctx = tercenCtx())  %>% 
+  select(.cindex, .rindex, .values) %>% 
+  reshape2::acast(.cindex ~ .rindex, value.var='.y', fun.aggregate=mean) %>%
+  Rtsne::Rtsne(dims = as.integer(ctx$op.value('dims')),
+               initial_dims = as.integer(ctx$op.value('initial_dims')),
+               perplexity = as.integer(ctx$op.value('perplexity')),
+               theta = as.double(ctx$op.value('theta')),
+               check_duplicates = FALSE,
+               pca=as.logical(ctx$op.value('pca')), 
+               max_iter = as.integer(ctx$op.value('max_iter')),
+               pca_center=as.logical(ctx$op.value('pca_center')), 
+               pca_scale=as.logical(ctx$op.value('pca_scale')), 
+               stop_lying_iter = as.integer(ctx$op.value('stop_lying_iter')),
+               mom_switch_iter = as.integer(ctx$op.value('mom_switch_iter')),
+               momentum = as.double(ctx$op.value('momentum')),
+               final_momentum = as.double(ctx$op.value('final_momentum')),
+               eta = as.double(ctx$op.value('eta')),
+               exaggeration_factor = as.double(ctx$op.value('exaggeration_factor'))
+               ) %>%
+  (function(tsne) {
+    d = as_tibble(tsne$Y)
+    names(d)=paste('tsne', seq_along(d), sep = '.')
+    return(d)
+  }) %>% 
+  mutate(.cindex = seq_len(nrow(.))-1) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
